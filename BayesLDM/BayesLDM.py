3a0d7eedcb2e8fc13e4a37de0980cf7518c65b0b
karine <76127656+karineML@users.noreply.github.com>
2022-09-11 15:52:06 -0400

    add files
    add files

--- BayesLDM/BayesLDM.py
+++ BayesLDM/BayesLDM.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpyro
 import numpyro.distributions as dist
 import numpyro.diagnostics as diagnostics
-from numpyro.infer import MCMC, NUTS
+from numpyro.infer import MCMC, NUTS, Predictive
 from numpyro.contrib.control_flow import scan
 from jax import random
 import jax.numpy as jnp
@@ -334,7 +334,6 @@ class compile:
     self.fitted_mcmc = None
     self.r_hat_warnings = ''
     self.chosen_implementation = ''
-    self.df_config = None
 
   def condition(self, obs, data):
     self.initialize(obs, data)
@@ -387,6 +386,7 @@ class compile:
         mcmc.print_summary()
       if b_display:
         print('\n'+str_duration+'\n')
+      self.model_code = model
       samples = mcmc.get_samples()
       self.unprocessed_samples = samples
       if b_post_process:
@@ -1299,44 +1299,13 @@ class compile:
       post_processed_samples = samples.copy() 
     return post_processed_samples
 
-  def get_imputed_df(self, old_df, sample_indices=[], method=None, rescale_df_info=None):
-    # select sample_indices for imputation, or set method to "mean" or "mode"
-    # if mean or mode is used, then get_imputed_df() returns imputed_df,
-    # otherwise, get_imputed_df() returns imputed_dfs_dict, with the sample_indices as keys
-    posterior_samples = self.post_process_samples(self.unprocessed_samples)
-    imputed_dfs_dict = {}
-    if len(sample_indices) > 0:
-      check_range = len(sample_indices)
-    else:
-      check_range = 1
-    for i in range(check_range):     
-      imputed_df = old_df.copy()    
-      for var_name in imputed_df.columns:
-        if rescale_df_info is not None:
-          df_mean = rescale_df_info['df_mean']
-          df_sd   = rescale_df_info['df_sd']
-          k = var_name        
-          imputed_df[var_name] = np.array(imputed_df[var_name]) * df_sd[k] + df_mean[k]
-          for name in posterior_samples.keys():
-            if name.find(k) >= 0:
-              posterior_samples[name] = np.array(posterior_samples[name]) * df_sd[k] + df_mean[k]   
-        found_posterior_nan_indices = []
-        if var_name in list(posterior_samples.keys()):
-          if len(sample_indices) > 0:
-            sample_index = sample_indices[i]           
-            imputed_samples = posterior_samples[var_name][:,sample_index]
-            imputed_dfs_dict[sample_index] = imputed_df
-          elif method == "mode":
-            last_dim_index = len(posterior_samples[var_name].shape)-1
-            imputed_samples = np.array(stats.mode(posterior_samples[var_name], axis=last_dim_index)[0].flatten())
-          else:
-            imputed_samples = posterior_samples[var_name].mean(axis=-1)            
-          data_nan_indices=list(np.argwhere(np.isnan(imputed_df[var_name]).values).flatten())
-          #found_posterior_nan_indices=list(np.argwhere(~np.isnan(imputed_samples)).flatten())
-          #assert(found_posterior_nan_indices == data_nan_indices), "nan indices in df and samples do not match"
-          imputed_df[var_name].values[data_nan_indices] = imputed_samples[data_nan_indices]
-    if len(sample_indices) > 0:
-      return imputed_dfs_dict
-    else:  
-      return imputed_df
-
+  def find_df(self, variable_name, b_display=True):
+    found_df = None
+    for df_i in self.data:
+      if variable_name in df_i:
+        found_df = df_i
+        break
+    if found_df is None:
+      if b_display:
+        print('no df was found for', variable_name)
+    return found_df
